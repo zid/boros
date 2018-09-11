f60f2e72b7b46c5da3bf48dcd6a1c3a1d08f7ce7
Peter Johnson <zidsoft@gmail.com>
2018-09-11 02:38:32 +0100

    cpu.asm: Add 32bit in and out instructions

--- cpu.asm
+++ cpu.asm
@@ -1,4 +1,5 @@
 global get_cr3, set_cr3, outb
+global out32, in32
 
 get_cr3:
 	mov rax, cr3
@@ -8,8 +9,20 @@ set_cr3:
 	mov rax, rdi
 	mov cr3, rax
 	ret
+
 outb:
 	mov rdx, rdi
 	mov rax, rsi
 	out dx, al
+	ret
+
+out32:
+	mov rdx, rdi
+	mov rax, rsi
+	out dx, eax
+	ret
+
+in32:
+	mov rdx, rdi
+	in eax, dx
 	ret
--- include/cpu.h
+++ include/cpu.h
@@ -1,5 +1,8 @@
-#ifndef  CPU_H
+#ifndef CPU_H
 #define CPU_H
+#include "types.h"
 unsigned long get_cr3(void);
-void set_cr3(unsigned long);
+void set_cr3(u64);
+void out32(u16, u32);
+u32 in32(u16);
 #endif
