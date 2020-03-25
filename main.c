ba667b65532a3f572d82c03440b3e869cd44e83d
Peter Johnson <zidsoft@gmail.com>
2020-03-25 23:04:37 +0000

    main: Run userspace
    Main now replaces the GDT with its own version, as to allow the TSS
    to work, then does an iretq to userspace.

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <int.h>
 #include <pci.h>
 #include <acpi.h>
+#include <gdt.h>
 
 void kmain(void *mem)
 {
@@ -12,12 +13,25 @@ void kmain(void *mem)
 	clear_screen(0);
 	printf("Hello from long mode\n");
 
+	gdt_install();
 	int_install();
-	//acpi_init();
+	/* acpi_init(); - PCI-E not delivering interrupts yet */
 	pci_init();
 
-	while(1)
-	{
-		asm("hlt");
-	}
+	/* No infrastructure for this exists yet, hardcoded here */
+	/* Drop into usermode */
+	asm(
+		"mov eax, 0x23;" /* User data seg + cpl3 */
+		"mov ds, ax;"
+		"mov es, ax;"
+		"mov fs, ax;"
+		"mov gs, ax;"
+		"push rax;" 
+		"push 0;" /* Blank RSP for now, no user stack */ 
+		"pushf;"  /* Leak our flags I guess */
+		"mov eax, 0x1B;" /* User code seg + cpl3 */
+		"push rax;"
+		"push 0x200000;"
+		"iretq;"
+	);
 }
